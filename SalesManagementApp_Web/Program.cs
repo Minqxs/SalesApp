7d653c2bcdba6284efb3ca7f2059a304af9e9236
LeonMaringa <leon@stackworx.io>
2024-10-18 21:48:41 +0200

    Another one

---
+++ SalesManagementApp_Web/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using SalesManagementApp_Core.DataAccess;
+using SalesManagementApp_Core.Graphql;
+using SalesManagementApp_Core.Interfaces;
+using SalesManagementApp_Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+var AllowSpecificOrigins = "_allowSpecificOrigins";
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("InMemoryDb");
+});
+builder.Services.AddScoped<AppDbContextService>();
+builder.Services.AddHttpClient<ApiRetrieverService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSpecificOrigins,
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddFiltering();
+
+var app = builder.Build();
+app.UseCors(AllowSpecificOrigins);
+app.MapGraphQL();
+using (var scope = app.Services.CreateScope())
+{
+    var apiRetrieverService = scope.ServiceProvider.GetRequiredService<ApiRetrieverService>();
+
+    // Run the methods to save products and sales on startup
+    await apiRetrieverService.MapAndSaveProducts();
+    await apiRetrieverService.MapAndSaveProductSales();
+}
+
+app.Run();
+
